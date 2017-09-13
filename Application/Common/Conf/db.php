4b76ae7658cae8da5807edeb3e85644debc42914
Administrator <zzhangxxiaonning@163.com>
2017-09-14 00:37:11 +0800

    01

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -16,14 +16,14 @@ return array(
 	
 	
 	'DB_TYPE'	=> 'mysql',		// 数据库类型
-//	'DB_HOST'	=> '118.190.134.66',	// 服务器地址
-//    'DB_NAME'	=> 'tp_magazines',    //数据库名称
-//    'DB_USER'	=> 'root',		// 用户名
-//    'DB_PWD'	=> 'ibangoo.com',			// 密码
-    'DB_HOST'	=> 'localhost',	// 服务器地址
+	'DB_HOST'	=> '118.190.134.66',	// 服务器地址
     'DB_NAME'	=> 'tp_jxufe',    //数据库名称
     'DB_USER'	=> 'root',		// 用户名
-    'DB_PWD'	=> '',			// 密码
+    'DB_PWD'	=> 'ibangoo.com',			// 密码
+//    'DB_HOST'	=> 'localhost',	// 服务器地址
+//    'DB_NAME'	=> 'tp_jxufe',    //数据库名称
+//    'DB_USER'	=> 'root',		// 用户名
+//    'DB_PWD'	=> '',			// 密码
     'DB_PORT'	=> '3306',		// 端口
     'DB_PREFIX' => 'cms_',
 
